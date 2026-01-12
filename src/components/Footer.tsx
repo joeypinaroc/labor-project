8e8b3ad440ebcb791dfe1e4ee1e3aabe2b01bd45
joeypinaroc <jose.pinaroc@easytechinnovations.com>
2026-01-11 17:35:08 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-
+import { Facebook, Instagram } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#4D4D4D] text-white">
+    <footer className="border-t border-gray-200 bg-white">
       <div className="container mx-auto px-4 py-12">
         <span>2026 All rights reserved</span>
       </div>
